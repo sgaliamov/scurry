33b90a9475bec706c54237292f2b66eba1418850
s. galiamov <sgalyamov@gmail.com>
2018-07-20 22:57:42 +0200

    draft of curry builder

---
+++ SCurry.Builders/CurryBuilder.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+
+namespace SCurry.Builders
+{
+    public static class CurryBuilder
+    {
+        private const ushort MaxInputArgumentsCount = 16;
+
+        /// <summary>
+        /// Generates all extensions.
+        /// </summary>
+        /// <param name="count">Number of methods starting (with 1) with parameterless function (Func&lt;TResult&gt;).</param>
+        /// <returns>All extension methods.</returns>
+        public static string GenerateAllFuncExtentions(ushort count = MaxInputArgumentsCount + 1) =>
+            ShortRange(0, count)
+                .Select(GenerateFuncExtention)
+                .Aggregate(new StringBuilder(), AppendLine)
+                .ToString();
+
+        public static string ReturnType(ushort level) => "Func<T1, Func<T2, Func<T3, TResult>>>";
+
+        public static string GenerateFuncExtention(ushort count)
+        {
+            var typeParameters = TypeParameters(count);
+
+            return $"public static {ReturnType(count)} Curry<{typeParameters}>"
+                   + $"(this Func<{typeParameters}> func) => {Body(count)};";
+        }
+
+        public static string Body(ushort count) => throw new NotImplementedException();
+
+        public static string TypeParameters(ushort count)
+        {
+            if (count == 0) return "TResult";
+
+            return string.Join(
+                       ", ",
+                       ShortRange(1, count)
+                           .Select(x => $"T{x.ToString(CultureInfo.InvariantCulture)}")
+                   ) + "TResult";
+        }
+
+        private static IEnumerable<ushort> ShortRange(ushort start, ushort count)
+        {
+            unchecked
+            {
+                for (var i = start; i < count + start; i++) yield return i;
+            }
+        }
+
+        private static StringBuilder AppendLine(StringBuilder sb, string method) => sb.AppendLine(method);
+    }
+}
---
+++ SCurry.Tests/CurryBuilderTests.cs
@@ -0,0 +1,100 @@
+﻿using SCurry.Builders;
+using Xunit;
+
+namespace SCurry.Tests
+{
+    public class CurryBuilderTests
+    {
+        [Fact]
+        public void Arguments_Test()
+        {
+            var actual = CurryBuilder.GenerateFuncExtention(3);
+
+            const string result = "public static Func<T1, Func<T2, Func<T3, TResult>>> "
+                                  + "Curry<T1, T2, T3, TResult>(this Func<T1, T2, T3, TResult> func) "
+                                  + "=> a => b => c => func(a, b, c);";
+
+            Assert.Equal(result, actual);
+        }
+
+        [Fact]
+        public void Body_Test()
+        {
+            var actual = CurryBuilder.GenerateFuncExtention(3);
+
+            const string result = "public static Func<T1, Func<T2, Func<T3, TResult>>> "
+                                  + "Curry<T1, T2, T3, TResult>(this Func<T1, T2, T3, TResult> func) "
+                                  + "=> a => b => c => func(a, b, c);";
+
+            Assert.Equal(result, actual);
+        }
+
+        [Fact]
+        public void GenerateAllFuncExtentions_0_Test()
+        {
+            var actual = CurryBuilder.GenerateAllFuncExtentions(0);
+
+            Assert.Equal("", actual);
+        }
+
+        [Fact]
+        public void GenerateAllFuncExtentions_3_Test()
+        {
+            var actual = CurryBuilder.GenerateAllFuncExtentions(3);
+
+            const string result = "public static Func<T1, Func<T2, Func<T3, TResult>>> "
+                                  + "Curry<T1, T2, T3, TResult>(this Func<T1, T2, T3, TResult> func) "
+                                  + "=> a => b => c => func(a, b, c);";
+
+            Assert.Equal(result, actual);
+        }
+
+        [Fact]
+        public void GenerateFuncExtention_0_Test()
+        {
+            var actual = CurryBuilder.GenerateFuncExtention(0);
+
+            const string result = "public static Func<TResult> "
+                                  + "Curry<TResult>(this Func<TResult> func) "
+                                  + "=> func;";
+
+            Assert.Equal(result, actual);
+        }
+
+        [Fact]
+        public void GenerateFuncExtention_3_Test()
+        {
+            var actual = CurryBuilder.GenerateFuncExtention(3);
+
+            const string result = "public static Func<T1, Func<T2, Func<T3, TResult>>> "
+                                  + "Curry<T1, T2, T3, TResult>(this Func<T1, T2, T3, TResult> func) "
+                                  + "=> a => b => c => func(a, b, c);";
+
+            Assert.Equal(result, actual);
+        }
+
+        [Fact]
+        public void ReturnType_Test()
+        {
+            var actual = CurryBuilder.GenerateFuncExtention(3);
+
+            const string result = "public static Func<T1, Func<T2, Func<T3, TResult>>> "
+                                  + "Curry<T1, T2, T3, TResult>(this Func<T1, T2, T3, TResult> func) "
+                                  + "=> a => b => c => func(a, b, c);";
+
+            Assert.Equal(result, actual);
+        }
+
+        [Fact]
+        public void TypeParameters_Test()
+        {
+            var actual = CurryBuilder.GenerateFuncExtention(3);
+
+            const string result = "public static Func<T1, Func<T2, Func<T3, TResult>>> "
+                                  + "Curry<T1, T2, T3, TResult>(this Func<T1, T2, T3, TResult> func) "
+                                  + "=> a => b => c => func(a, b, c);";
+
+            Assert.Equal(result, actual);
+        }
+    }
+}
