e1297a6afee0e85bb3759455e3e8464aaa4a21a9
s. galiamov <sgalyamov@gmail.com>
2018-07-29 21:09:16 +0200

    packaging

--- clean.ps1
+++ clean.ps1
@@ -1,5 +1,12 @@
+param(
+    [switch][Alias("c")]$cleanCode
+)
+
 Remove-Item ".\*\obj\" -Force -Recurse
 Remove-Item ".\*\bin\" -Force -Recurse
-Get-ChildItem ".\*.tt" -Recurse | ForEach-Object {
-    Remove-Item $([io.path]::ChangeExtension($_.FullName, "cs")) -Force -ErrorAction Ignore
+
+if ($cleanCode) {
+    Get-ChildItem ".\*.tt" -Recurse | ForEach-Object {
+        Remove-Item $([io.path]::ChangeExtension($_.FullName, "cs")) -Force -ErrorAction Ignore
+    }
 }
