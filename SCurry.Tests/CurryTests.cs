7567b8744b69e32c25cf2a889f19740b63a60e37
s. galiamov <sgalyamov@gmail.com>
2018-07-29 19:39:29 +0200

    added generated code

---
+++ SCurry.Tests/CurryTests.cs
@@ -0,0 +1,185 @@
+﻿/*
+   ___ ___ _  _ ___ ___    _ _____ ___ ___     ___ ___  ___  ___ 
+  / __| __| \| | __| _ \  /_\_   _| __|   \   / __/ _ \|   \| __|
+ | (_ | _|| .` | _||   / / _ \| | | _|| |) | | (_| (_) | |) | _| 
+  \___|___|_|\_|___|_|_\/_/ \_\_| |___|___/   \___\___/|___/|___|
+
+*/
+
+using Xunit; 
+
+namespace SCurry.Tests
+{
+    public class CurryTests
+    {
+        [Fact]
+        public void Curry1_Test()
+        {
+            var curryed = TestFunctions.Id.Curry();
+
+            var actual = curryed(1);
+
+            Assert.Equal(1, actual);
+        }
+
+        [Fact]
+        public void Curry2_Test()
+        {
+            var curryedAdd = TestFunctions.Add2.Curry();
+
+            var actual = curryedAdd(1)(2);
+
+            Assert.Equal(3, actual);
+        }
+
+        [Fact]
+        public void Curry3_Test()
+        {
+            var curryedAdd = TestFunctions.Add3.Curry();
+
+            var actual = curryedAdd(1)(2)(3);
+
+            Assert.Equal(6, actual);
+        }
+
+        [Fact]
+        public void Curry4_Test()
+        {
+            var curryedAdd = TestFunctions.Add4.Curry();
+
+            var actual = curryedAdd(1)(2)(3)(4);
+
+            Assert.Equal(10, actual);
+        }
+
+        [Fact]
+        public void Curry5_Test()
+        {
+            var curryedAdd = TestFunctions.Add5.Curry();
+
+            var actual = curryedAdd(1)(2)(3)(4)(5);
+
+            Assert.Equal(15, actual);
+        }
+
+        [Fact]
+        public void Curry6_Test()
+        {
+            var curryedAdd = TestFunctions.Add6.Curry();
+
+            var actual = curryedAdd(1)(2)(3)(4)(5)(6);
+
+            Assert.Equal(21, actual);
+        }
+
+        [Fact]
+        public void Curry7_Test()
+        {
+            var curryedAdd = TestFunctions.Add7.Curry();
+
+            var actual = curryedAdd(1)(2)(3)(4)(5)(6)(7);
+
+            Assert.Equal(28, actual);
+        }
+
+        [Fact]
+        public void Curry8_Test()
+        {
+            var curryedAdd = TestFunctions.Add8.Curry();
+
+            var actual = curryedAdd(1)(2)(3)(4)(5)(6)(7)(8);
+
+            Assert.Equal(36, actual);
+        }
+
+        [Fact]
+        public void Curry9_Test()
+        {
+            var curryedAdd = TestFunctions.Add9.Curry();
+
+            var actual = curryedAdd(1)(2)(3)(4)(5)(6)(7)(8)(9);
+
+            Assert.Equal(45, actual);
+        }
+
+        [Fact]
+        public void Curry10_Test()
+        {
+            var curryedAdd = TestFunctions.Add10.Curry();
+
+            var actual = curryedAdd(1)(2)(3)(4)(5)(6)(7)(8)(9)(10);
+
+            Assert.Equal(55, actual);
+        }
+
+        [Fact]
+        public void Curry11_Test()
+        {
+            var curryedAdd = TestFunctions.Add11.Curry();
+
+            var actual = curryedAdd(1)(2)(3)(4)(5)(6)(7)(8)(9)(10)(11);
+
+            Assert.Equal(66, actual);
+        }
+
+        [Fact]
+        public void Curry12_Test()
+        {
+            var curryedAdd = TestFunctions.Add12.Curry();
+
+            var actual = curryedAdd(1)(2)(3)(4)(5)(6)(7)(8)(9)(10)(11)(12);
+
+            Assert.Equal(78, actual);
+        }
+
+        [Fact]
+        public void Curry13_Test()
+        {
+            var curryedAdd = TestFunctions.Add13.Curry();
+
+            var actual = curryedAdd(1)(2)(3)(4)(5)(6)(7)(8)(9)(10)(11)(12)(13);
+
+            Assert.Equal(91, actual);
+        }
+
+        [Fact]
+        public void Curry14_Test()
+        {
+            var curryedAdd = TestFunctions.Add14.Curry();
+
+            var actual = curryedAdd(1)(2)(3)(4)(5)(6)(7)(8)(9)(10)(11)(12)(13)(14);
+
+            Assert.Equal(105, actual);
+        }
+
+        [Fact]
+        public void Curry15_Test()
+        {
+            var curryedAdd = TestFunctions.Add15.Curry();
+
+            var actual = curryedAdd(1)(2)(3)(4)(5)(6)(7)(8)(9)(10)(11)(12)(13)(14)(15);
+
+            Assert.Equal(120, actual);
+        }
+
+        [Fact]
+        public void Curry16_Test()
+        {
+            var curryedAdd = TestFunctions.Add16.Curry();
+
+            var actual = curryedAdd(1)(2)(3)(4)(5)(6)(7)(8)(9)(10)(11)(12)(13)(14)(15)(16);
+
+            Assert.Equal(136, actual);
+        }
+
+        [Fact]
+        public void Curry0_Test()
+        {
+            var curryed = TestFunctions.Zero.Curry();
+
+            var actual = curryed();
+
+            Assert.Equal(0, actual);
+        }
+    }
+}
---
+++ SCurry.Tests/TestFunctions.cs
@@ -0,0 +1,47 @@
+﻿/*
+   ___ ___ _  _ ___ ___    _ _____ ___ ___     ___ ___  ___  ___
+  / __| __| \| | __| _ \  /_\_   _| __|   \   / __/ _ \|   \| __|
+ | (_ | _|| .` | _||   / / _ \| | | _|| |) | | (_| (_) | |) | _|
+  \___|___|_|\_|___|_|_\/_/ \_\_| |___|___/   \___\___/|___/|___|
+
+*/
+using System;
+
+namespace SCurry.Tests
+{
+    public static class TestFunctions
+    {
+        public static readonly Func<int> Zero = () => 0;
+        public static readonly Func<int, int> Id = (int i) => i;
+        public static readonly Func<int, int, int> Add2 = (int arg1, int arg2) => arg1 + arg2;
+
+        public static readonly Func<int, int, int, int> Add3 = (int arg1, int arg2, int arg3) => arg1 + arg2 + arg3;
+
+        public static readonly Func<int, int, int, int, int> Add4 = (int arg1, int arg2, int arg3, int arg4) => arg1 + arg2 + arg3 + arg4;
+
+        public static readonly Func<int, int, int, int, int, int> Add5 = (int arg1, int arg2, int arg3, int arg4, int arg5) => arg1 + arg2 + arg3 + arg4 + arg5;
+
+        public static readonly Func<int, int, int, int, int, int, int> Add6 = (int arg1, int arg2, int arg3, int arg4, int arg5, int arg6) => arg1 + arg2 + arg3 + arg4 + arg5 + arg6;
+
+        public static readonly Func<int, int, int, int, int, int, int, int> Add7 = (int arg1, int arg2, int arg3, int arg4, int arg5, int arg6, int arg7) => arg1 + arg2 + arg3 + arg4 + arg5 + arg6 + arg7;
+
+        public static readonly Func<int, int, int, int, int, int, int, int, int> Add8 = (int arg1, int arg2, int arg3, int arg4, int arg5, int arg6, int arg7, int arg8) => arg1 + arg2 + arg3 + arg4 + arg5 + arg6 + arg7 + arg8;
+
+        public static readonly Func<int, int, int, int, int, int, int, int, int, int> Add9 = (int arg1, int arg2, int arg3, int arg4, int arg5, int arg6, int arg7, int arg8, int arg9) => arg1 + arg2 + arg3 + arg4 + arg5 + arg6 + arg7 + arg8 + arg9;
+
+        public static readonly Func<int, int, int, int, int, int, int, int, int, int, int> Add10 = (int arg1, int arg2, int arg3, int arg4, int arg5, int arg6, int arg7, int arg8, int arg9, int arg10) => arg1 + arg2 + arg3 + arg4 + arg5 + arg6 + arg7 + arg8 + arg9 + arg10;
+
+        public static readonly Func<int, int, int, int, int, int, int, int, int, int, int, int> Add11 = (int arg1, int arg2, int arg3, int arg4, int arg5, int arg6, int arg7, int arg8, int arg9, int arg10, int arg11) => arg1 + arg2 + arg3 + arg4 + arg5 + arg6 + arg7 + arg8 + arg9 + arg10 + arg11;
+
+        public static readonly Func<int, int, int, int, int, int, int, int, int, int, int, int, int> Add12 = (int arg1, int arg2, int arg3, int arg4, int arg5, int arg6, int arg7, int arg8, int arg9, int arg10, int arg11, int arg12) => arg1 + arg2 + arg3 + arg4 + arg5 + arg6 + arg7 + arg8 + arg9 + arg10 + arg11 + arg12;
+
+        public static readonly Func<int, int, int, int, int, int, int, int, int, int, int, int, int, int> Add13 = (int arg1, int arg2, int arg3, int arg4, int arg5, int arg6, int arg7, int arg8, int arg9, int arg10, int arg11, int arg12, int arg13) => arg1 + arg2 + arg3 + arg4 + arg5 + arg6 + arg7 + arg8 + arg9 + arg10 + arg11 + arg12 + arg13;
+
+        public static readonly Func<int, int, int, int, int, int, int, int, int, int, int, int, int, int, int> Add14 = (int arg1, int arg2, int arg3, int arg4, int arg5, int arg6, int arg7, int arg8, int arg9, int arg10, int arg11, int arg12, int arg13, int arg14) => arg1 + arg2 + arg3 + arg4 + arg5 + arg6 + arg7 + arg8 + arg9 + arg10 + arg11 + arg12 + arg13 + arg14;
+
+        public static readonly Func<int, int, int, int, int, int, int, int, int, int, int, int, int, int, int, int> Add15 = (int arg1, int arg2, int arg3, int arg4, int arg5, int arg6, int arg7, int arg8, int arg9, int arg10, int arg11, int arg12, int arg13, int arg14, int arg15) => arg1 + arg2 + arg3 + arg4 + arg5 + arg6 + arg7 + arg8 + arg9 + arg10 + arg11 + arg12 + arg13 + arg14 + arg15;
+
+        public static readonly Func<int, int, int, int, int, int, int, int, int, int, int, int, int, int, int, int, int> Add16 = (int arg1, int arg2, int arg3, int arg4, int arg5, int arg6, int arg7, int arg8, int arg9, int arg10, int arg11, int arg12, int arg13, int arg14, int arg15, int arg16) => arg1 + arg2 + arg3 + arg4 + arg5 + arg6 + arg7 + arg8 + arg9 + arg10 + arg11 + arg12 + arg13 + arg14 + arg15 + arg16;
+
+    }
+}
