308c51fd0b3b8b2cd23a770019a4b9a7f2520464
s. galiamov <sgalyamov@gmail.com>
2018-10-14 16:10:57 +0200

    TypeParametersConverter and CurryReturnTypeConverter

---
+++ SCurry.Builders/Converters/CurryReturnTypeConverter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+using SCurry.Builders.Shared;
+
+namespace SCurry.Builders.Converters
+{
+    internal sealed class CurryReturnTypeConverter : IConverter
+    {
+        public string Convert(MarkerFlags markers)
+        {
+            var argsCount = markers.Flags.Length;
+
+            if (argsCount == 1 && markers.Type == MarkersType.Action)
+            {
+                return "Action<T1>";
+            }
+
+            var result = markers.Type == MarkersType.Action
+                ? $"Action<T{argsCount}>"
+                : "TResult";
+
+            return markers.Flags
+                .Select(x => $"Func<T{x}, ")
+                .Append(result)
+                .Concat(Enumerable.Range(0, argsCount).Select(_ => ">"))
+                .Join();
+        }
+    }
+}
---
+++ SCurry.Builders/Converters/TypeParametersConverter.cs
@@ -0,0 +1,23 @@
+﻿using System.Globalization;
+using System.Linq;
+using SCurry.Builders.Shared;
+
+namespace SCurry.Builders.Converters
+{
+    internal sealed class TypeParametersConverter : IConverter
+    {
+        public string Convert(MarkerFlags markers)
+        {
+            var types = markers.Flags
+                .Where(x => x)
+                .Select(x => $"T{x.ToString(CultureInfo.InvariantCulture)}");
+
+            if (markers.Type == MarkersType.Function)
+            {
+                types = types.Append("TResult");
+            }
+
+            return types.Join(", ");
+        }
+    }
+}
