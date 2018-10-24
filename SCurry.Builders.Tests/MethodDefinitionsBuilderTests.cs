a485e0f16366ea7f3df2b5d3e9426cbe8179fcfc
s. galiamov <sgalyamov@gmail.com>
2018-10-25 00:04:04 +0200

    configured constants

--- SCurry.Builders.Tests/MethodDefinitionsBuilderTests.cs
+++ SCurry.Builders.Tests/MethodDefinitionsBuilderTests.cs
@@ -115,7 +115,8 @@ namespace SCurry.Builders.Tests
                 new[] { 1, 1, 0, 0, 0 },
                 new[] { 0, 0, 1, 0, 0 },
                 new[] { 1, 0, 1, 0, 0 },
-                new[] { 0, 1, 1, 0, 0 }
+                new[] { 0, 1, 1, 0, 0 },
+                new[] { 1, 1, 1, 0, 0 }
             };
             var expected = Convert(markers, type);
 
