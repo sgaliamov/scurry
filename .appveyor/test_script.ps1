e001fd2e8eac6deaf069472a4619bed4d25dacd3
s. galiamov <sgalyamov@gmail.com>
2018-10-25 00:32:41 +0200

    test coverage only for develop

--- .appveyor/test_script.ps1
+++ .appveyor/test_script.ps1
@@ -1,4 +1,4 @@
-if ($env:APPVEYOR_REPO_BRANCH -in "develop", "master") {
+if ($env:APPVEYOR_REPO_BRANCH -eq "develop") {
     Get-ChildItem .\**\*.Tests.csproj -Recurse | ForEach-Object {
         .\OpenCover.4.6.519\tools\OpenCover.Console.exe `
             -register:user `
