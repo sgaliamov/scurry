f795346eecd9e0917ddc53b08c5b7afacb24b2e3
s. galiamov <sgalyamov@gmail.com>
2018-10-25 00:26:09 +0200

    fixing build

--- .appveyor/test_script.ps1
+++ .appveyor/test_script.ps1
@@ -3,7 +3,7 @@ if ($env:APPVEYOR_REPO_BRANCH -in "develop", "master") {
         .\OpenCover.4.6.519\tools\OpenCover.Console.exe `
             -register:user `
             -target:"C:\Program Files\dotnet\dotnet.exe" `
-            -targetargs:"test $_ --no-build -c debug" `
+            -targetargs:"test $_ --no-build -c $env:CONFIGURATION" `
             -output:"coverage.xml" `
             -oldstyle `
 			-returntargetcode
