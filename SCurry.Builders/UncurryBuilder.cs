d0eac39a0a7641ea849e2df9cccb9bb60ba5be88
s. galiamov <sgalyamov@gmail.com>
2018-10-26 00:14:28 +0200

    created UncurryBuilder

--- SCurry.Builders/UncurryBuilder.cs
+++ SCurry.Builders/UncurryBuilder.cs
@@ -1,4 +1,38 @@
-﻿namespace SCurry.Builders
+﻿using System.Collections.Generic;
+using System.Linq;
+using SCurry.Builders.Converters;
+using SCurry.Builders.Models;
+
+namespace SCurry.Builders
 {
-    public sealed class UncurryBuilder { }
+    public sealed class UncurryBuilder
+    {
+        private readonly IMethodDefinitionsBuilder _definitionsBuilder;
+        private readonly IConverter _methodConverter;
+
+        public UncurryBuilder(IConverter converter, IMethodDefinitionsBuilder definitionsBuilder)
+        {
+            _methodConverter = converter;
+            _definitionsBuilder = definitionsBuilder;
+        }
+
+        public IEnumerable<string> GenerateFuncExtensions(int maxArgsCount) =>
+            Generate(MethodType.Function, maxArgsCount);
+
+        public IEnumerable<string> GenerateActionExtensions(int maxArgsCount) =>
+            Generate(MethodType.Action, maxArgsCount);
+
+        private IEnumerable<string> Generate(MethodType methodType, int maxArgsCount)
+        {
+            for (var argsCount = 0; argsCount <= maxArgsCount; argsCount++)
+            {
+                var definition = _definitionsBuilder
+                                 .Build(methodType, 0, argsCount, 0)
+                                 .Select(_methodConverter.Convert)
+                                 .Single();
+
+                yield return definition;
+            }
+        }
+    }
 }
