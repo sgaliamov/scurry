18647d5d70c0fe271f8375f951be15d6e0dcec95
s. galiamov <sgalyamov@gmail.com>
2018-10-29 14:39:04 +0100

    added new environment for nuget org

--- .appveyor/before_deploy.ps1
+++ .appveyor/before_deploy.ps1
@@ -2,7 +2,8 @@
 
 if ($env:APPVEYOR_REPO_BRANCH -eq "master") {
     dotnet pack .\SCurry\ --include-source --include-symbols --no-build --no-dependencies
+
     Get-ChildItem ".\SCurry\**\*.nupkg" -Recurse | ForEach-Object {
-        Push-AppveyorArtifact $_.FullName -FileName $_.Name -DeploymentName "appveyor-nuget"
+        Push-AppveyorArtifact $_.FullName -FileName $_.Name -DeploymentName "nuget-package"
     }
 }
