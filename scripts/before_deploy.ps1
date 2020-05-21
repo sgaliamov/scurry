5d4eca7796937169029413caf994719492611f08
s. galiamov <sgalyamov@gmail.com>
2020-05-21 23:05:13 +0200

    fix

--- scripts/before_deploy.ps1
+++ scripts/before_deploy.ps1
@@ -1,7 +1,7 @@
 if ($env:APPVEYOR_REPO_BRANCH -eq "master") {
     dotnet pack --include-source --include-symbols --no-build --no-dependencies
 
-    Get-ChildItem ".\**\*.nupkg" -Recurse | ForEach-Object {
-        Push-AppveyorArtifact $_.FullName -FileName $_.Name -DeploymentName "nuget-package"
-    }
+    # Get-ChildItem ".\**\*.nupkg" -Recurse | ForEach-Object {
+    #     Push-AppveyorArtifact $_.FullName -FileName $_.Name -DeploymentName "nuget-package"
+    # }
 }
