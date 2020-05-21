84475eb42eacce20de3d355bfd6bad1468e21898
s. galiamov <sgalyamov@gmail.com>
2020-05-21 20:41:05 +0200

    F

--- src/SCurry.Tests/PartialCurryTests.cs
+++ src/SCurry.Tests/PartialCurryTests.cs
@@ -11,15 +11,17 @@ namespace SCurry.Tests
         [Fact]
         public void Partial_WithGaps_Then_Curry()
         {
-            var partial = Foo.Partial(_, _, 2, _, 3);
+
+            var partial = F(Foo).Partial(_, _, 2, _, 3);
             var curried = partial.Curry();
 
             Assert.Equal(
                 partial(1, 6, 7),
                 curried(1)(6)(7));
         }
+        
+        static int Foo(int a, int b, int c, int d, int e) => a + b * (c + d - e);
 
-        private static readonly Func<int, int, int, int, int, int> Foo = (a, b, c, d, e) =>
-            a + b * (c + d - e);
+        public static Func<int, int, int, int, int, int> F(Func<int, int, int, int, int, int> foo) => foo;
     }
 }
