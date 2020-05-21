fdb1624077db0de464e020e26c157ad25ca03e36
s. galiamov <sgalyamov@gmail.com>
2020-05-21 20:46:29 +0200

    fixed clean

--- scripts/clean.ps1
+++ scripts/clean.ps1
@@ -1,11 +1,11 @@
 param(
     [switch][Alias("c")]$cleanCode
 )
-
-Remove-Item \obj, \bin -Force -Recurse
+  
+Get-ChildItem -Include bin, obj -Recurse | Remove-Item -Force -Recurse
 
 if ($cleanCode) {
-    Get-ChildItem ".\*\*.tt" -Recurse | ForEach-Object {
+    Get-ChildItem ".\**\*.tt" -Recurse | ForEach-Object {
         Remove-Item $([io.path]::ChangeExtension($_.FullName, "generated.cs")) -Force -ErrorAction Ignore
     }
 }
